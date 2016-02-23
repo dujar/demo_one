28ed678bb742940a00e727408e5c6a37f7e6fd8f
dujar <dujar.coding@gmail.com>
2016-02-23 13:37:28 +0100

    added puppies.rb

---
+++ puppies.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+#
+# puppies.rb
+# Copyright (C) 2016 Fab <Fab@pc57.home>
+#
+# Distributed under terms of the MIT license.
+#
+
+
+class Puppies
+
+
+    def hello
+    end
+end
+
